63c1b5cf99bf6488c91b689e54d188aa6f8eef70
有澤孝輝 <teruuuuun72@gmail.com>
2020-08-05 16:42:53 +0900

    [Add] validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,12 +12,19 @@ class BooksController < ApplicationController
   end
 
   def new
+    book = Book.new(book_params)
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      logger.debug(@book.errors.to_yaml)
+      @books = Book.all
+      # @book = Book.new
+      render 'books/index'
+    end
   end
 
   def edit
